e6d7329a5eb7a1946956cb41c77cdc99b354f547
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-02-23 18:49:36 +0530

    initial
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/bin
+
+# Setup kubernetes cluster
+kind create cluster
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/bin
+
+# Spin up the containers
+docker-compose -d up
